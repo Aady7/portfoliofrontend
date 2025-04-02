f498d5dea2f79a11b0c40c0fb5943a62cd46d44b
Aady <pratapadityasingh2703@gmail.com>
2025-04-03 02:31:15 +0530

    refactor: remove unused ButtonComponent to clean up codebase

--- src/components/button.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {Button} from 'reactstrap';
-
-export default function ButtonComponent() {
-    return (
-        <Button color="primary" className=''>Hello World</Button>)}
